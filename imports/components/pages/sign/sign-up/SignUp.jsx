5809f0af0a6436b5ccf30c7b09c0fff81e21be69
Денис Сыроежкин <kol9743@gmail.com>
2017-04-08 14:53:27 +0200

    Sign message fix

--- imports/components/pages/sign/sign-up/SignUp.jsx
+++ imports/components/pages/sign/sign-up/SignUp.jsx
@@ -105,26 +105,28 @@ export default class SignUp extends Component {
               </div>
               <Form size={'tiny'} onSubmit={this.handleSubmit}>
                 <div className="login-item">
-                	{message ? 
-                  	<Message size='tiny' negative>
-                     	<Message.Header>{message}</Message.Header>
-                     	{username ? <p>{username}</p> : null}
-                     	{email ? <p>{email}</p> : null}
-                     	{password ? <p>{password}</p> : null}
-                  	</Message>
-    				 	: null}
+                {message ? 
+                  <Message size='tiny'>
+                    <Message.Header>{message}</Message.Header>
+                    <Message.List>
+                    {username ?  <Message.Item>{username}</Message.Item> : null}
+                    {email ?  <Message.Item>{email}</Message.Item> : null}
+                    {password ? <Message.Item>{password}</Message.Item> : null}
+                    </Message.List>
+                  </Message>
+                : null}
                 </div>
                 <div className="login-item">
-                  <Form.Input icon='checkmark' label='Ваше имя:' name='name' type="text" placeholder='Елена Петровна' error={username ? true : false} required/>
+                  <Form.Input label='Ваше имя:' name='name' type="text" placeholder='Елена Петровна' error={username ? true : false}/>
                 </div>
                 <div className="login-item">
-                  <Form.Input label='E-mail:' name='email' type="email" placeholder='example@mail.com' error={email ? true : false} required/>
+                  <Form.Input label='E-mail:' name='email' type="email" placeholder='example@mail.com' error={email ? true : false}/>
                 </div>
                 <div className="login-item">
-                  <Form.Input label='Пароль' name='password' type="password" placeholder='Ваш пароль' error={password ? true : false} required/>
+                  <Form.Input label='Пароль' name='password' type="password" placeholder='Ваш пароль' error={password ? true : false}/>
                 </div>
                 <div className="login-item">
-                  <Form.Input label='Повторите пароль' name='passwordR' type="password" placeholder='Пароль еще раз' required/>
+                  <Form.Input label='Повторите пароль' name='passwordR' type="password" placeholder='Пароль еще раз'/>
                 </div>
                 <div className="login-item">
                   <button type="submit" className="simple-btn simple-btn_blue">Зарегистрироваться</button>
